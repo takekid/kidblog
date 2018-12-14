fd00454b5d0f438ca0d34b3395bfa57960edc777
takekid <kid@kidnoMacBook-puro.local>
2018-12-14 10:37:20 +0900

    Blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'blogs#index'
-  get 'blogs' => 'blogs#index'
-  get 'blogs/new' => 'blogs#new'
-  post 'blogs' => 'blogs#create'
-  delete 'blogs/:id' => 'blogs#destroy'
-  patch 'blogs/:id' => 'blogs#update'
-  get 'blogs/:id/edit' => 'blogs#edit'
-  get 'users/:id' => 'users#show'
-  get 'blogs/:id' => 'blogs#show'
+  root "blogs#index"
+  resources :blogs, only: [:index, :show, :new, :destroy, :edit, :update, :create]
+  resources :users, only: [:show]
 end
